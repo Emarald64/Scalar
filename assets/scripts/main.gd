baf4b00c043a7aa8408f29d591d7217a9cebe649
Alex Giller <alexgiller33@gmail.com>
2024-08-17 17:14:01 -0400

    Locked window aspect ratio

--- assets/scripts/main.gd
+++ assets/scripts/main.gd
@@ -2,6 +2,12 @@ extends Node2D
 
 @onready var window=get_viewport()
 
+#@onready var PreviousSize = window.size
+@onready var defaultSize = Vector2(window.size)
+ 
+#var ignore_Y = false
+#var ignore_X = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,7 +15,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Player.scale=Vector2.ONE*1000/window.size.y
-	$Player.speed=500000/window.size.y
-	$Player.jump_velocity=-900000/window.size.y
-	$Camera2D.zoom=Vector2.ONE*window.size.y/1000
+	var scale = Vector2(window.size) / defaultSize
+	
+	$Player.scale=Vector2.ONE/(min(scale.x,scale.y)**2)
+	$Player.speed=500/min(scale.x,scale.y)
+	$Player.jump_velocity=-500/min(scale.x,scale.y)
+	$Camera2D.zoom=Vector2.ONE*min(scale.x,scale.y)
