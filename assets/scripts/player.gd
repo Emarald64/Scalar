333dc6049f3f2410a89d29aaa95139563ddf2ea1
Alex Giller <alexgiller33@gmail.com>
2024-08-18 14:49:28 -0400

    Added momentum and controls tutorial

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -10,18 +10,14 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	elif Input.is_action_just_pressed("jump"):
 		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+	velocity.x= lerp(velocity.x,direction*speed,(15 if is_on_floor() else 5) * delta)
 
 	move_and_slide()
 
